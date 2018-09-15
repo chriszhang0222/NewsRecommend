3a5c432beca4bff07ea70339c3576ec5353290c3
chris <869415122@qq.com>
2018-09-15 16:57:36 -0700

    ui change

--- tap-news/src/App.js
+++ tap-news/src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+          <img className='logo' src={logo} alt='logo'/>
+          <div className='container'>
+
+          </div>
       </div>
     );
   }
