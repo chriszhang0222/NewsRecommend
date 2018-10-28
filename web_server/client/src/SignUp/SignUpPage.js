bca4c174a9fe1a0171d7306573b47d347b141d38
chris <869415122@qq.com>
2018-10-28 16:45:41 -0700

    signup added

--- web_server/client/src/SignUp/SignUpPage.js
+++ web_server/client/src/SignUp/SignUpPage.js
@@ -1,62 +1,61 @@
 import React from 'react';
-import Auth from '../Auth/Auth';
-import LoginForm from './LoginForm';
 import PropTypes from 'prop-types';
+import SignUpForm from './SignUpForm';
 
-class LoginPage extends React.Component {
+class SignUpPage extends React.Component {
     constructor(props, context) {
         super(props, context);
 
+        // set the initial component state
         this.state = {
             errors: {},
             user: {
                 email: '',
-                password: ''
+                password: '',
+                confirm_password: ''
             }
         };
-
         this.processForm = this.processForm.bind(this);
         this.changeUser = this.changeUser.bind(this);
     }
 
     processForm(event){
         event.preventDefault();
-
         const email = this.state.user.email;
         const password = this.state.user.password;
+        const confirm_password = this.state.user.confirm_password;
 
-        console.log(email);
-        console.log(password);
+        if(password !== confirm_password){
+            return;
+        }
 
-        fetch('http://localhost:3000/auth/login', {
+        fetch('http://localhost:3000/auth/signup', {
             method:'POST',
             cache:false,
             headers:{
                 'Accept':'application/json',
-                'Content-Type': 'application/json',
+                'Content-Type':'application/json'
             },
             body:JSON.stringify({
-                email: this.state.user.email,
+                email:this.state.user.email,
                 password: this.state.user.password
             })
         }).then(response => {
             if(response.status === 200){
                 this.setState({errors:{}});
+                this.context.router.replace('/');
+            }
+            else{
                 response.json().then(function(json){
                     console.log(json);
-                    Auth.authenticateUser(json.token, email);
-                    this.context.router.replace('/');
-                }.bind(this));
-            }
-            else {
-                console.log('Login Failed');
-                response.json().then(function (json) {
-                   const errors = json.errors ? json.errors : {};
+                    const errors = json.errors ? json.errors : {};
                     errors.summary = json.message;
+                    console.log(this.state.errors);
                     this.setState({ errors });
                 }.bind(this));
             }
-        });
+        })
+
     }
 
     changeUser(event){
@@ -67,18 +66,21 @@ class LoginPage extends React.Component {
         this.setState({
             user
         });
-    }
 
+        if (this.state.user.password !== this.state.user.confirm_password) {
+            const errors = this.state.errors;
+            errors.password = "Password and Confirm Password don't match.";
+            this.setState({ errors });
+        } else {
+            const errors = this.state.errors;
+            errors.password = '';
+            this.setState({ errors });
+        }
+    }
+    
     render(){
         return (
-            <LoginForm
-                onSubmit={this.processForm()}
-                onChange={this.changeUser()}
-                errors={this.state.errors}
-                user={this.state.user}
-            />
+            <SignUpForm onSubmit={this.processForm()} onChange={this.changeUser()} errors={this.state.errors} user={this.state.user}/>
         )
     }
 }
-
-export default LoginPage;
