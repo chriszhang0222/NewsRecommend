7056facde7997e23d68cc265e0e008245dadeb02
chriszhang94 <chris@quantumsds.com>
2020-09-26 09:01:34 -0400

    seaech init

--- web_server/client/src/Base/Base.js
+++ web_server/client/src/Base/Base.js
@@ -1,15 +1,17 @@
 import React, {PropTypes} from 'react';
+import { Link } from 'react-router';
 import Auth from '../Auth/Auth';
 import './Base.css';
 
 const Base = ({children}) => (
     <div>
-        <nav className="nav-bar indigo lighten-1">
+        <nav className="nav">
             <div className="nav-wrapper">
                 <a href="/" className="brand-logo">&nbsp;&nbsp;News Recommend</a>
                 <ul id="nav-mobile" className="right">
                     {Auth.isUserAuthenticated() ?
                             (<div>
+                                <li><Link to='/search'><i className="fa fa-search"></i> Search News</Link></li>
                                 <li>{Auth.getEmail()}</li>
                                 <li><a href="/logout">Log out</a></li>
                             </div>)
